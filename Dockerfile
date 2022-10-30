0c70c49c488931b3bd822502da1b7ae69fdbd39d
QQ304988350 <304988350@qq.com>
2022-10-30 15:09:19 +0000

    重新部署

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM registry.cn-hangzhou.aliyuncs.com/304988350/tool_man:latest
 
+RUN chmod +x start.sh
+
 COPY ./ /app/tool_man
---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pip install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple
+
+
+cd /app/tool_man && bash start.sh
